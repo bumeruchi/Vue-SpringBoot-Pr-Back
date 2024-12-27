49f7456488d5e65682061468ccac9614dd0bb1c0
bumeruchi <jack1871@naver.com>
2024-12-27 17:40:10 +0900

    1

---
+++ src/main/java/com/example/demo/controller/BoardController.java
@@ -0,0 +1,48 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.BoardDto;
+import com.example.demo.entity.BoardEntity;
+import com.example.demo.model.Header;
+import com.example.demo.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@CrossOrigin
+@RestController
+public class BoardController {
+    private final BoardService boardService;
+
+    @GetMapping("/board/list")
+    public Header<List<BoardDto>> boardList(@PageableDefault(sort = {"idx"}) Pageable pageable
+    ) {
+
+        return boardService.getBoardList(pageable);
+    }
+
+    @GetMapping("/board/{id}")
+    public BoardDto getBoard(@PathVariable Long id) {
+        return boardService.getBoard(id);
+    }
+
+    @PostMapping("/board")
+    public BoardEntity create(@RequestBody BoardDto boardDto) {
+        return boardService.create(boardDto);
+    }
+
+    @PatchMapping("/board")
+    public BoardEntity update(@RequestBody BoardDto boardDto) {
+        return boardService.update(boardDto);
+    }
+
+    @DeleteMapping("/board/{id}")
+    public void delete(@PathVariable Long id) {
+        boardService.delete(id);
+    }
+}
